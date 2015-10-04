e02139e0fc4b70ba6f4423db372d1eea4e7597a2
sa7mon <dan.sa7mon@gmail.com>
2015-10-04 06:36:15 +0000

    Add error handling on reader initialization

--- senseloop.py
+++ senseloop.py
@@ -12,7 +12,9 @@ import nfc
 
 ###############   FUNCTIONS  #################
 
-# Start id 
+# idToVid
+# Takes a tag ID and checks it against an array of arrays with items set up as:
+# (tagID, video) and returns the video.
 def idToVid(id):
     # Give the return variable a default value of error.
     # Hopefully, this doesn't get returned.
@@ -25,7 +27,6 @@ def idToVid(id):
     # Loop through the array, checking the first item (0) of the sub-array
     # If it matches, return the second item (1)
     for entry in videos:
-        # Do something
 	    if (entry[0] == id):
                video = entry[1]
     return video
@@ -39,16 +40,21 @@ def printTag(tag):
     print "Video: ", idToVid(tagID)
     return
 
-###############   MAIN PROGRAM   ################## 
+##################   MAIN PROGRAM   ####################### 
 
 # Setup our reader connection through UART
 # To verify from Python CLI, print(clf) should result in /dev/AMA0
 print "Initiating reader..."
-clf = nfc.ContactlessFrontend('tty')
-
-# Continuously listen for tags nearby. Fire an event when we see one.
-loop = 1; # Make this not 1 if loop needs to break
-while loop == 1: 
-    print "Waiting for tag to read..."
-    clf.connect(rdwr={'on-connect': printTag})
+try:
+    clf = nfc.ContactlessFrontend('tto')
+except IOError:
+    print "Couldn't initialize reader (IOError)"
+else: 
+    
+    # Continuously listen for tags nearby. Fire an event when we see one.
+    # Make loop not 1 if loop needs to break
+    loop = 1; 
+    while loop == 1: 
+        print "Waiting for tag to read..."
+        clf.connect(rdwr={'on-connect': printTag})
 
