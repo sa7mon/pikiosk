5545bf333fb321e401796cfe92cfb6ca64be9a75
sa7mon <dan.sa7mon@gmail.com>
2015-10-04 05:31:36 +0000

    idToVid: cleanup arrays, parse array more cleanly using 2D arrays, add comments. printTag: cleanup printing. Whole program: add comments/section breaks.

--- senseloop.py
+++ senseloop.py
@@ -1,45 +1,54 @@
 #!/usr/bin/env python
+
+####
 #
 # Written by: Dan Salmon
 # Created on: 10/02/15 
 #
+####
+
+################   IMPORTS   #################
 import nfc 
 
-#IDtoVideo
+###############   FUNCTIONS  #################
+
+# Start id 
 def idToVid(id):
-    videolookup = (
+    # Give the return variable a default value of error.
+    # Hopefully, this doesn't get returned.
+    video = "unknown"
+    videos = (
 		   ("04C023D24C2880", "video1"), 
 		   ("048D495A002980", "video2"), 
 		   ("3", "video3")
 		  )
-    videolookup2 = ("04C023D24C2880:video1", "048D495A002980:video2")
-    
-    # Loop through the array, checking each item to see if it starts with the ID we were given.
-    for entry in videolookup2:
+    # Loop through the array, checking the first item (0) of the sub-array
+    # If it matches, return the second item (1)
+    for entry in videos:
         # Do something
-	entryid = entry.split(":", 2)[0] # Should give us just the string before the colon
-	entryvideo = entry.split(":", 2)[1]
-	print "entryid: ", entryid # DEBUG
-	print "entryvideo: ", entryvideo # DEBUG
-
-    return
+	    if (entry[0] == id):
+               video = entry[1]
+    return video
 
 # printTag
 # function that gets the id we need. Runs only when on-connect event is raised.
 def printTag(tag):
     # We're only interested in the ID. 
     tagID = str(tag).split("ID=", 1)[1]
-    print tagID
-    print idToVid(id)
+    print "Tag ID: ", tagID
+    print "Video: ", idToVid(tagID)
     return
 
-print "Initiating reader..."
+###############   MAIN PROGRAM   ################## 
+
 # Setup our reader connection through UART
 # To verify from Python CLI, print(clf) should result in /dev/AMA0
+print "Initiating reader..."
 clf = nfc.ContactlessFrontend('tty')
 
-loop = 1;
 # Continuously listen for tags nearby. Fire an event when we see one.
+loop = 1; # Make this not 1 if loop needs to break
 while loop == 1: 
     print "Waiting for tag to read..."
     clf.connect(rdwr={'on-connect': printTag})
+
