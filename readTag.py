00e6a3e4cb29359be8049f342b2a0dee7ba25ecc
sa7mon <dan.sa7mon@gmail.com>
2015-10-09 01:54:58 +0000

    Create simple tag reader program

---
+++ readTag.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#####
+# Created by: Dan Salmon
+# Created on: 10/8/15
+#####
+
+################   IMPORTS   #################
+import nfc 
+
+################  FUNCTIONS  #################
+
+# printTag
+# function that gets the id we need. Runs only when on-connect event is raised.
+def printTag(tag):
+    # We're only interested in the ID. 
+    tagID = str(tag).split("ID=", 1)[1]
+    print "Tag ID: ", tagID
+    return
+
+def readTag():
+    # Continuously listen for tags nearby. Fire an event when we see one.
+    # Make loop not 1 if loop needs to break
+    waitingForTag = 1
+    while waitingForTag == 1: 
+        print "Waiting for tag to read..."
+        clf.connect(rdwr={'on-connect': printTag})
+        
+    return
+
+############## MAIN PROGRAM  #################
+
+# Setup our reader connection through UART
+# To verify from Python CLI, print(clf) should result in /dev/AMA0
+print "Initiating reader..."
+try:
+    #clf = nfc.ContactlessFrontend('tty')
+    print "Reader initiated." #DEBUG?
+except IOError:
+    print "Couldn't initialize reader (IOError)"
+else: 
+    
+    quit = False
+    
+    while quit == False:
+        # Loop through menu here
+        menu = "1. Read a tag's ID\n2. Quit"
+        print menu
+        
+        userInput = raw_input("Enter a choice: ")
+        
+        if userInput == "1":
+            print "I'm number 1!"
+        elif userInput == "2":
+            print "I'm number 2!"
+        else:
+            print "Not a valid choice. Please try again."
+        
+        
