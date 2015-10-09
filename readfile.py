bb0091ecaad5deab43a6bb50bdc0fc47c2ac409d
sa7mon <dan.sa7mon@gmail.com>
2015-10-09 03:09:50 +0000

    Add if/else to determine what to split from the text file based on mode.

--- readfile.py
+++ readfile.py
@@ -4,18 +4,25 @@
 # File needs to be in key=value format
 
 def read(file, mode):
-	if not ((mode == "videos") or (mode == "lights")):
-		print "bad mode, friend"
-
+	# Initialize a blank dictionary to fill and then return
 	readDict = {}
+	
+	# Check to make sure the mode passed is one of the approved 2
+	if not ((mode == "videos") or (mode == "lights")):
+		print "Error: read(): Bad mode passed"
+		return readDict
+	
+	# Open the file as text
 	with open(file, "r") as text:
+		# For each line in the test file, do the following
+            	# Split the line into 2 lines delimited by the equal sign
+            	# Make the first string the dict key and the second the dict value
 		for line in text:
-			# For each line in the test file, do the following
-            		# Split the line into 2 lines delimited by the equal sign
-            		# Make the first string the dict key and the second the dict value
-			readDict[line.split('=', 2)[0]] = line.split('=', 2)[1].strip('\n')
-		# print readDict
+			if (mode == "videos"):
+				readDict[line.split('=', 2)[0]] = line.split('=', 2)[1].strip('\n')
+			elif (mode == "lights"):
+				readDict[line.split('=', 3)[0]] = line.split('=', 3)[2].strip('\n')
 	return readDict 
 
-myDict = read("videos.txt", "lights")
+myDict = read("videoscolors.txt", "lights")
 print myDict
