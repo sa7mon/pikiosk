89b52f154648b17b777d9c86b2aa94c15cd556a8
sa7mon <dan.sa7mon@gmail.com>
2015-10-13 16:51:45 +0000

    Add modified read() and change name to readFile() for clarification.  Comment out reading section to debug file reading. Seems to be working.

--- senseloop.py
+++ senseloop.py
@@ -12,23 +12,21 @@ import nfc
 
 ###############   FUNCTIONS  #################
 
+def closeReader():
+	# Close the reader
+	#nfc.closeReader(clf)
+	# Then set loop to not 1 to cleanly exit program instead of suspending it.
+	global loop 
+	loop = 0
+	return
+
 # idToVid
 # Takes a tag ID and checks it against an array of arrays with items set up as:
 # (tagID, video) and returns the video.
 def idToVid(id):
     # Give the return variable a default value of error.
     # Hopefully, this doesn't get returned.
-    video = "unknown"
-    videos = (
-		   ("04C023D24C2880", "video1"), 
-		   ("048D495A002980", "video2"), 
-		   ("3", "video3")
-		  )
-    # Loop through the array, checking the first item (0) of the sub-array
-    # If it matches, return the second item (1)
-    for entry in videos:
-	    if (entry[0] == id):
-               video = entry[1]
+
     return video
 
 # printTag
@@ -42,29 +40,49 @@ def printTag(tag):
 
 # readFile
 # Reads a file into a dictionary
-# File needs to be in key=value format
-def readFile(file):
-        readDict = {}
-        with open(file, "r") as text:
-                for line in text:
-                        # For each line in the test file, do the following
-            		# Split the line into 2 lines delimited by the equal sign
-            		# Make the first string the dict key and the second the dict value
-                        readDict[line.split('=', 2)[0]] = line.split('=', 2)[1].strip('\n')
-                #print readDict # DEBUG
-        return readDict
+#
+# file data needs to be in 'key=video=color' format
+# mode can be either "videos" or "lights"
+def readFile(file, mode):
+	# Initialize a blank dictionary to fill and then return
+	readDict = {}
+	
+	# Check to make sure the mode passed is one of the approved 2
+	if not ((mode == "videos") or (mode == "lights")):
+		print "Error: read(): Bad mode passed"
+		return readDict
+	
+	# Open the file as text
+	with open(file, "r") as text:
+		# For each line in the test file, do the following
+            	# Split the line into 2 lines delimited by the equal sign
+            	# Make the first string the dict key and the second the dict value
+		for line in text:
+			if (mode == "videos"):
+				readDict[line.split('=', 2)[0]] = line.split('=', 2)[1].strip('\n')
+			elif (mode == "lights"):
+				readDict[line.split('=', 3)[0]] = line.split('=', 3)[2].strip('\n')
+	return readDict 
 
 ##################   MAIN PROGRAM   ####################### 
 
 # Read the file with videos and tag IDs into a dict
 print "Reading videos file..."
-videoDict = readFile("videos.txt")
+videoDict = readFile("videoscolors.txt", "videos")
+lightsColorsDict = readFile("videoscolors.txt", "lights")
+
+#DEBUG
+print videoDict
+print lightsColorsDict
+
 
 # Setup our reader connection through UART
 # To verify from Python CLI, print(clf) should result in /dev/AMA0
+
+'''
 print "Initiating reader..."
 try:
-    clf = nfc.ContactlessFrontend('tto')
+    clf = nfc.ContactlessFrontend('tty')
 except IOError:
     print "Couldn't initialize reader (IOError)"
 else: 
@@ -75,4 +93,5 @@ else:
     while loop == 1: 
         print "Waiting for tag to read..."
         clf.connect(rdwr={'on-connect': printTag})
+'''
 
