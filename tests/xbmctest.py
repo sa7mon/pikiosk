a9eac13c1a932a2bf275182afa99c21f58a73c2f
Dan Salmon <dan.sa7mon@gmail.com>
2015-10-18 20:59:34 -0500

    Create working demo of manipulating playlist while video from it is playing. A key breakthrough

--- tests/xbmctest.py
+++ tests/xbmctest.py
@@ -23,7 +23,83 @@ def executeRPC(rpcdata):
 payload1 = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
 payload2 = {"jsonrpc": "2.0","id": "1", "method":"player.open", "params": {"item": {"file" : "/home/osmc/Movies/Sample1.mp4"}, "options": {"repeat": "all"}}}
 payload3 = {"jsonrpc": "2.0", "id": "1", "method": "JSONRPC.Version"}
+payload4 = {
+	"jsonrpc": "2.0",
+	"id":	1,
+	"method": "Playlist.Add",
+	"params": {
+		"playlistid": 1,
+		"item": {
+			"file": "/home/osmc/Videos/Sample1.mp4"
+		} 
+	}
+}
+plClearPlist = {
+	"jsonrpc": "2.0",
+	"id": 1,
+	"method": "Playlist.Clear",
+	"params": {
+		"playlistid": 1
+	}
+}
+payload5= {
+	"jsonrpc": "2.0",
+	"id": 1,
+	"method": "Playlist.Add",
+	"params": {
+		"playlistid": 1,
+		"item": {
+			"file": "/home/osmc/Videos/Sample-Standby.mp4"
+		}
+	}
+}
+payload6 = {
+	"jsonrpc": "2.0",
+	"id": 1,
+	"method": "Player.Open",
+	"params": {
+		"item": {
+			"playlistid": 1
+		}
+	}
+}
 
+payload7 = {
+	"jsonrpc": "2.0",
+	"id": 1,
+	"method": "Playlist.GetItems",
+	"params": {
+		"playlistid": 1
+	}
+}
 
-print "Response:"
-print executeRPC(payload2).text
+plPlayerRepeat = {
+	"jsonrpc": "2.0",
+	"id": 1,
+	"method": "Player.SetRepeat",
+	"params": {
+		"playerid": 1,
+		"repeat": "all"
+	}
+}
+
+print "Clearing playlist"
+print executeRPC(plClearPlist).text
+
+print "Adding first video to playlist"
+print executeRPC(payload4).text
+
+print "Opening player"
+print executeRPC(payload6).text
+
+print "Getting playlist info"
+print executeRPC(payload7).text
+
+print "Clearing playlist"
+print executeRPC(plClearPlist).text
+
+print "Adding standby video to playlist"
+print executeRPC(payload5).text
+
+print "Setting repeat to one"
+print executeRPC(plPlayerRepeat).text
