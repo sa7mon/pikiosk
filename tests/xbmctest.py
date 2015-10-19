1c406107b7c4e4d2bfc5a50a42c0dc1ba1a94659
Dan Salmon <dan.sa7mon@gmail.com>
2015-10-19 14:03:10 -0500

    Simplify executeRPC() and consolidate payloads

--- tests/xbmctest.py
+++ tests/xbmctest.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+
 import requests
 import json
 import urllib
 
-def executeRPC(rpcdata):
-	headers = {'content-type': 'application/json'}
+def executeRPC(rpcpayload):
 	# Kodi config #
 	kodi_host = 'localhost'
 	kodi_port = 80
@@ -13,78 +13,23 @@ def executeRPC(rpcdata):
 	#parameter. For POSTs, we add the payload as JSON the the HTTP request body
 	kodi_json_rpc_url = "http://" + kodi_host + ":" + str(kodi_port) + "/jsonrpc"
 	
-	url_param = urllib.urlencode({'request': json.dumps(rpcdata)}) 
+	url_param = urllib.urlencode({'request': json.dumps(rpcpayload)}) 
 	
-	return requests.get(kodi_json_rpc_url + '?' + url_param,headers=headers)
-
-
+	return requests.get(kodi_json_rpc_url + '?' + url_param,headers={'content-type': 'application/json'})
 
 #Payload for the method to get the currently playing / paused video or audio
 payload1 = {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
 payload2 = {"jsonrpc": "2.0","id": "1", "method":"player.open", "params": {"item": {"file" : "/home/osmc/Movies/Sample1.mp4"}, "options": {"repeat": "all"}}}
 payload3 = {"jsonrpc": "2.0", "id": "1", "method": "JSONRPC.Version"}
-payload4 = {
-	"jsonrpc": "2.0",
-	"id":	1,
-	"method": "Playlist.Add",
-	"params": {
-		"playlistid": 1,
-		"item": {
-			"file": "/home/osmc/Videos/Sample1.mp4"
-		} 
-	}
-}
-plClearPlist = {
-	"jsonrpc": "2.0",
-	"id": 1,
-	"method": "Playlist.Clear",
-	"params": {
-		"playlistid": 1
-	}
-}
-payload5= {
-	"jsonrpc": "2.0",
-	"id": 1,
-	"method": "Playlist.Add",
-	"params": {
-		"playlistid": 1,
-		"item": {
-			"file": "/home/osmc/Videos/Sample-Standby.mp4"
-		}
-	}
-}
-payload6 = {
-	"jsonrpc": "2.0",
-	"id": 1,
-	"method": "Player.Open",
-	"params": {
-		"item": {
-			"playlistid": 1
-		}
-	}
-}
-
-payload7 = {
-	"jsonrpc": "2.0",
-	"id": 1,
-	"method": "Playlist.GetItems",
-	"params": {
-		"playlistid": 1
-	}
-}
-
-plPlayerRepeat = {
-	"jsonrpc": "2.0",
-	"id": 1,
-	"method": "Player.SetRepeat",
-	"params": {
-		"playerid": 1,
-		"repeat": "all"
-	}
-}
+payload4 = {"jsonrpc": "2.0","id":	1,"method": "Playlist.Add","params": {"playlistid": 1,"item": {"file": "/home/osmc/Videos/Sample1.mp4"}}}
+plClearPlaylist = {"jsonrpc": "2.0","id": 1,"method": "Playlist.Clear","params": {"playlistid": 1}}
+payload5= {"jsonrpc": "2.0","id": 1,"method": "Playlist.Add","params": {"playlistid": 1,"item": {"file": "/home/osmc/Videos/Sample-Standby.mp4"}}}
+payload6 = {"jsonrpc": "2.0","id": 1,"method": "Player.Open","params": {"item": {"playlistid": 1}}}
+payload7 = {"jsonrpc": "2.0","id": 1,"method": "Playlist.GetItems","params": {"playlistid": 1}}
+plPlayerRepeat = {"jsonrpc": "2.0","id": 1,"method": "Player.SetRepeat","params": {"playerid": 1,"repeat": "all"}}
 
 print "Clearing playlist"
-print executeRPC(plClearPlist).text
+print executeRPC(plClearPlaylist).text
 
 print "Adding first video to playlist"
 print executeRPC(payload4).text
