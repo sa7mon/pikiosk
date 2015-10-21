1b30d28c57a6333b51ec28a3fc00feecd6809d59
Dan Salmon <dan.sa7mon@gmail.com>
2015-10-20 22:36:47 -0500

    Modify test to study on_connect, on_release relationship. Found out that simply returning True from on_connect causes the clf.connect() call to block.

--- tests/testnfc.py
+++ tests/testnfc.py
@@ -6,34 +6,62 @@ import nfc
 
 print "Starting..."
 
-clftty = nfc.ContactlessFrontend('tty:AMA0:pn532')
+clftty = nfc.ContactlessFrontend('tty')
 
 # If everything is working, this should print: '   on /dev/ttyAMA0'
 #print clftty
 
 def on_connect(tag):
-	print "on_connect: fired"
-	print(tag)
+	#print "Tag has connected."
+	print "Starting on_connect..."
+
+	tagID = str(tag).split("ID=", 1)[1]
+	# DEBUG - Check for kill tag
+	if (str(tagID) == "046B435A002980"):
+		# Reading this tag should kill the program cleanly.
+		print "Kill tag read. Shutting script down..."
+		closeReader()
+		return
+
+	print "Exiting on_connect."
+	return True
 
 def on_startup(targets):
 	# Fires every single time clftty.connect is fired
 	print "on_startup: fired"
+	return
 
-	#loopme = True
-	#while loopme == True:
-		# Do something
-	#	print " "
-	
-	
+def on_release(tag):
+	print "Tag has released."	
+	return True
+
+def closeReader():
+	global clftty
+	global loop 
+
+	# Close the reader
+	clftty.close()
+
+	loop = False
 	return
 
 rdwr_options = {
 	'on-connect': on_connect,
+	'on-release': on_release,
+	#'on-startup': on_startup,
 }
 
 loop = True
 while loop == True:
-	print "Top of the loop"
-	tag = clftty.connect(rdwr=rdwr_options)
+	#print "Top of the loop"
+	if (on_connect):
+		print "on_connect = True"
+	elif not (on_connect):
+		print "on_connect = False"
+	elif (on_release):
+		print "on_release = True"
+	else:
+		print "Got to here"
 
-#tag = clftty.connect(rdwr=rdwr_options)
+	tag = clftty.connect(rdwr=rdwr_options)
+	print "clftty.connect(): fired"
