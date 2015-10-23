6aa9fe4a510e3652d40a8a9009ea66b1fc2daced
Dan Salmon <dan.sa7mon@gmail.com>
2015-10-22 22:57:32 -0500

    Start linting. Went from -12/10 to 5.2/10

--- senseloop.py
+++ senseloop.py
@@ -3,7 +3,7 @@
 #################################
 #
 # Written by: Dan Salmon
-# Created on: 10/02/15 
+# Created on: 10/02/15
 #
 #################################
 
@@ -16,106 +16,112 @@ import nfc, requests, json, urllib
 ###############   FUNCTIONS  #################
 
 def closeReader():
-	global clf
-	global loop 
+    """ Close the reader and stop watching for tags. """
+    # Close the reader
+    clf.close()
 
-	# Close the reader
-	clf.close()
-
-	# Then set loop to not 1 to cleanly exit program instead of suspending it.
-	loop = 0
-	return
+    # Then set loop to not 1 to cleanly exit program instead of suspending it.
+    loop = 0
+    return
 
 def executeRPC(rpcpayload):
-	# Kodi config #
-	kodi_host = 'localhost'
-	kodi_port = 80
- 
-	#Base URL of the json RPC calls. For GET calls we append a "request" URI 
-	#parameter. For POSTs, we add the payload as JSON the the HTTP request body
-	kodi_json_rpc_url = "http://" + kodi_host + ":" + str(kodi_port) + "/jsonrpc"
-	
-	url_param = urllib.urlencode({'request': json.dumps(rpcpayload)}) 
-	
-	return requests.get(kodi_json_rpc_url + '?' + url_param,headers={'content-type': 'application/json'})
+    # Kodi config #
+    kodi_host = 'localhost'
+    kodi_port = 80
+
+    #Base URL of the json RPC calls. For GET calls we append a "request" URI
+    #parameter. For POSTs, we add the payload as JSON the the HTTP request body
+    kodi_json_rpc_url = "http://" + kodi_host + ":" + str(kodi_port) + "/jsonrpc"
+
+    url_param = urllib.urlencode({'request': json.dumps(rpcpayload)})
+
+    return requests.get(kodi_json_rpc_url + '?' + url_param, headers={'content-type': 'application/json'})
 
 def on_connect(tag):
-	global videoDict
-	global lightsColorsDict
-	global dirVideos
-	global fileStandbyVideo
-	
-	# Get the tag's ID 
-	tagID = str(tag).split("ID=", 1)[1]
+    #global videoDict
+    #global lightsColorsDict
+    #global dirVideos
+    #global fileStandbyVideo
 
-	# DEBUG - Check for kill tag
-	if (str(tagID) == "046B435A002980"):
-		# Reading this tag should kill the program cleanly.
-		print "Kill tag read. Shutting script down..."
-		print executeRPC(plPlayerStop).text
-		closeReader()
-		return
+    # Get the tag's ID
+    tagID = str(tag).split("ID=", 1)[1]
 
-	# Check if the tag exists in both dictionaries
-	if not ((tagID in videoDict) and (tagID in lightsColorsDict)):
-		print "This tag doesn't exist in one of the dictionaries."
-		return True
-	else: 
-		#Get video filename to play: e.g. Sample1.mp4
-		video = videoDict.get(tagID)
+    # DEBUG - Check for kill tag
+    if str(tagID) == "046B435A002980":
+        # Reading this tag should kill the program cleanly.
+        print "Kill tag read. Shutting script down..."
+        print executeRPC(plPlayerStop).text
+        closeReader()
+        return
 
-		# Clear playlist
-		print "Clearing playlist"
-		print executeRPC(plPlaylistClear)
+    # Check if the tag exists in both dictionaries
+    if not ((tagID in videoDict) and (tagID in lightsColorsDict)):
+        print "This tag doesn't exist in one of the dictionaries."
+        return True
+    else:
+        #Get video filename to play: e.g. Sample1.mp4
+        video = videoDict.get(tagID)
 
-		# Add the appropriate video the playlist
-		print "Adding specific video to playlist"
-		plAddVideo = {"jsonrpc": "2.0","id": 1,"method": "Playlist.Add","params": {"playlistid": 1,"item": {"file": dirVideos + video}}}
-		print executeRPC(plAddVideo).text
+        # Clear playlist
+        print "Clearing playlist"
+        print executeRPC(plPlaylistClear)
 
-		# Open player
-		print "Opening player..."
-		print executeRPC(plPlayerOpen)
+        # Add the appropriate video the playlist
+        print "Adding specific video to playlist"
+        plAddVideo = {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "Playlist.Add",
+            "params": {
+                "playlistid": 1,
+                "item": {"file": dirVideos + video}
+            }
+        }
+        print executeRPC(plAddVideo).text
 
-		# Clear playlist
-		print "Clearing playlist"
-		print executeRPC(plPlaylistClear)
+        # Open player
+        print "Opening player..."
+        print executeRPC(plPlayerOpen)
 
-		# Add standby video to playlist
-		print "Adding standby video to playlist"
-		print executeRPC(plPlaylistAddStandby)
+        # Clear playlist
+        print "Clearing playlist"
+        print executeRPC(plPlaylistClear)
 
-		# Set player repeat to 'one'
-		print "Setting player repeat to one"
-		print executeRPC(plPlayerSetRepeat)
+        # Add standby video to playlist
+        print "Adding standby video to playlist"
+        print executeRPC(plPlaylistAddStandby)
 
-		return True
+        # Set player repeat to 'one'
+        print "Setting player repeat to one"
+        print executeRPC(plPlayerSetRepeat)
 
-def readFile(file, mode):
-	# readFile - Reads a file into a dictionary
-	#
-	# file data needs to be in 'key=video=color' format
-	# mode can be either "videos" or "lights"
+        return True
 
-	# Initialize a blank dictionary to fill and then return
-	readDict = {}
-	
-	# Check to make sure the mode passed is one of the approved 2
-	if not ((mode == "videos") or (mode == "lights")):
-		print "Error: read(): Bad mode passed"
-		return readDict
-	
-	# Open the file as text
-	with open(file, "r") as text:
-		# For each line in the test file, do the following
-		# Split the line into 2 lines delimited by the equal sign
-		# Make the first string the dict key and the second the dict value
-		for line in text:
-			if (mode == "videos"):
-				readDict[line.split('=', 2)[0]] = line.split('=', 2)[1].strip('\n')
-			elif (mode == "lights"):
-				readDict[line.split('=', 3)[0]] = line.split('=', 3)[2].strip('\n')
-	return readDict 
+def readFile(filename, mode):
+    # readFile - Reads a file into a dictionary
+    #
+    # file data needs to be in 'key=video=color' format
+    # mode can be either "videos" or "lights"
+
+    # Initialize a blank dictionary to fill and then return
+    readDict = {}
+
+    # Check to make sure the mode passed is one of the approved 2
+    if not ((mode == "videos") or (mode == "lights")):
+        print "Error: read(): Bad mode passed"
+        return readDict
+
+    # Open the file as text
+    with open(filename, "r") as text:
+        # For each line in the test file, do the following
+        # Split the line into 2 lines delimited by the equal sign
+        # Make the first string the dict key and the second the dict value
+        for line in text:
+            if mode == "videos":
+                readDict[line.split('=', 2)[0]] = line.split('=', 2)[1].strip('\n')
+            elif mode == "lights":
+                readDict[line.split('=', 3)[0]] = line.split('=', 3)[2].strip('\n')
+    return readDict
 
 ################# VARIABLES #######################
 
@@ -132,14 +138,42 @@ fileVideos = "videos.txt"
 fileStandbyVideo = "Sample-Standby.mp4"
 
 # JSON-RPC payloads to send to Kodi on localhost
-plPlaylistClear = {"jsonrpc": "2.0","id": 1,"method": "Playlist.Clear","params": {"playlistid": 1}}
-plPlaylistAddStandby = {"jsonrpc": "2.0","id": 1,"method": "Playlist.Add","params": {"playlistid": 1,"item": {"file": dirVideos + fileStandbyVideo}}}
-plPlayerOpen = {"jsonrpc": "2.0","id": 1,"method": "Player.Open","params": {"item": {"playlistid": 1}}}
-plPlayerSetRepeat = {"jsonrpc": "2.0","id": 1,"method": "Player.SetRepeat","params": {"playerid": 1,"repeat": "all"}}
-plPlayerStop = {"jsonrpc": "2.0","id": 1,"method": "Player.Stop","params": {"playerid": 1}}
+plPlaylistClear = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "Playlist.Clear",
+    "params": {"playlistid": 1}
+}
+plPlaylistAddStandby = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "Playlist.Add",
+    "params": {
+        "playlistid": 1,
+        "item": {"file": dirVideos + fileStandbyVideo}
+    }
+}
+plPlayerOpen = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "Player.Open",
+    "params": {"item": {"playlistid": 1}}
+}
+plPlayerSetRepeat = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "Player.SetRepeat",
+    "params": {"playerid": 1, "repeat": "all"}
+}
+plPlayerStop = {
+    "jsonrpc": "2.0",
+    "id": 1,
+    "method": "Player.Stop",
+    "params": {"playerid": 1}
+}
 
 
-##################   MAIN PROGRAM   ####################### 
+##################   MAIN PROGRAM   #######################
 
 # Read the file with videos and tag IDs into a dict
 print "Reading videos file..."
@@ -149,32 +183,32 @@ lightsColorsDict = readFile(fileVideos, "lights")
 
 # Setup our reader connection through UART
 # To diagnose issues:
-# 	To verify from Python CLI, print(clf) should result in /dev/AMA0
-# 	Alternately, 'tty:AMA0:pn532' will probably work in place of 'tty'
+#   To verify from Python CLI, print(clf) should result in /dev/AMA0
+#   Alternately, 'tty:AMA0:pn532' will probably work in place of 'tty'
 print "Initiating reader..."
 try:
-	clf = nfc.ContactlessFrontend('tty')
+    clf = nfc.ContactlessFrontend('tty')
 except IOError:
-	print "Couldn't initialize reader (IOError)"
-else: 
-	# Clear playlist
-	print "Clearing playlist"
-	print executeRPC(plPlaylistClear).text
+    print "Couldn't initialize reader (IOError)"
+else:
+    # Clear playlist
+    print "Clearing playlist"
+    print executeRPC(plPlaylistClear).text
 
-	# Add standby video to playlist
-	print "Adding standby video to playlist"
-	print executeRPC(plPlaylistAddStandby).text
+    # Add standby video to playlist
+    print "Adding standby video to playlist"
+    print executeRPC(plPlaylistAddStandby).text
 
-	# Set player repeat to 'one'
-	print "Setting player repeat to one"
-	print executeRPC(plPlayerSetRepeat).text
+    # Set player repeat to 'one'
+    print "Setting player repeat to one"
+    print executeRPC(plPlayerSetRepeat).text
 
-	# Open player
-	print "Opening player"
-	print executeRPC(plPlayerOpen).text 
-	
-	# Continuously listen for tags nearby. Fire an event when we see one.
-	# Make loop not 1 to stop waiting for tags.
-	while loop == 1:
-		print "Waiting for tag to read..."
-		clf.connect(rdwr={'on-connect': on_connect})
+    # Open player
+    print "Opening player"
+    print executeRPC(plPlayerOpen).text
+
+    # Continuously listen for tags nearby. Fire an event when we see one.
+    # Make loop not 1 to stop waiting for tags.
+    while loop == 1:
+        print "Waiting for tag to read..."
+        clf.connect(rdwr={'on-connect': on_connect})
